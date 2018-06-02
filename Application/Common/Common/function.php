ba75ba10687a18d519a8b713b92f738a0426b528
maying <maying@itv.baidu.com>
2018-06-02 11:27:04 +0800

    redis

---
+++ Application/Common/Common/function.php
@@ -0,0 +1,16 @@
+<?php
+    
+    function cache_set ($key, $value,$expire=3600) {
+        $redis = new Redis();
+        $redis->connect('192.168.199.249', 6379);
+        $value = serialize($value);
+        return $redis->setex($key, $expire, $value);  
+    }
+
+    function cache_get ($key) {
+        $redis = new Redis();
+        $redis->connect('192.168.199.249', 6379);
+        $value = $redis->get($key);  
+        $value = unserialize($value);
+        return $value;
+    }
